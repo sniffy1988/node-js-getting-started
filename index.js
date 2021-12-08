958f136d9d1cc7ce288cca626093318faf9c2b66
Anton Lobodenko <anton.lobodenko@1648factory.com>
2021-12-08 16:47:16 +0200

    [TASK] started work

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.get("/", (req, res) => res.render("pages/index"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get("/api", function (req, res) {
+  res.send({
+    status: 200,
+    valid: true,
+    foo: "bar",
+  });
+});
